4bcb51d67d9753b4887bbf89aec90b02d974b0c1
Kartik Chawla <kchawla1995@gmail.com>
2022-10-27 14:07:49 -0400

    Initial Commit

---
+++ Trex/TrexApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrexApp.swift
+//  Trex
+//
+//  Created by Kartik Chawla on 2022-10-27.
+//
+
+import SwiftUI
+
+@main
+struct TrexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
